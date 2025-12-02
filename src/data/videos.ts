9051b863199c8675b3f3788244480f918c56dd85
Elinfridrika1 <elinh21@ru.is>
2025-12-02 12:57:03 +0100

    fixing paths

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -5,13 +5,21 @@ export interface VideoStimulus {
 }
 
 // Generate 64 mock videos
+// const videoSrc = [
+//   "public/video/demo_video1-1.mp4",
+//   "public/video/demo_video2-1.mp4",
+//   "public/video/demo_video3-1.mp4",
+//   "public/video/demo_video4-1.mp4",
+//   "public/video/demo_video4-1.mp4",
+// ];
 const videoSrc = [
-  "public/video/demo_video1-1.mp4",
-  "public/video/demo_video2-1.mp4",
-  "public/video/demo_video3-1.mp4",
-  "public/video/demo_video4-1.mp4",
-  "public/video/demo_video4-1.mp4",
+  "/video/demo_video1-1.mp4",
+  "/video/demo_video2-1.mp4",
+  "/video/demo_video3-1.mp4",
+  "/video/demo_video4-1.mp4",
+  "/video/demo_video4-1.mp4",
 ];
+
 const shuffledVideos = [...videoSrc].sort(() => Math.random() - 0.5);
 export const videos: VideoStimulus[] = Array.from({ length: 5 }, (_, i) => ({
   id: `video-${i + 1}`,
